ed1be4a0181c804964c7814e3c2bdcff622ce00f
Lucas Doyle <lucas.p.doyle@gmail.com>
2014-04-23 20:11:32 +0900

    minor edits to grunt and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,12 @@
-/*
- * Generated on 2014-04-23
- * generator-assemble v0.4.11
- * https://github.com/assemble/generator-assemble
- *
- * Copyright (c) 2014 Hariadi Hinta
- * Licensed under the MIT license.
- */
-
 'use strict';
-
-// # Globbing
-// for performance reasons we're only matching one level down:
-// '<%= config.src %>/templates/pages/{,*/}*.hbs'
-// use this if you want to match all subfolders:
-// '<%= config.src %>/templates/pages/**/*.hbs'
 
 module.exports = function(grunt) {
 
   require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
+  grunt.loadNpmTasks('assemble');
+
   grunt.initConfig({
 
     config: {
@@ -28,19 +15,21 @@ module.exports = function(grunt) {
     },
 
     watch: {
+
       assemble: {
-        files: ['<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}'],
+        files: ['<%= config.src %>/{content,data,templates}/**/*.{md,hbs,yml}'],
         tasks: ['assemble']
       },
+
       livereload: {
         options: {
           livereload: '<%= connect.options.livereload %>'
         },
         files: [
-          '<%= config.dist %>/{,*/}*.html',
-          '<%= config.dist %>/assets/{,*/}*.css',
-          '<%= config.dist %>/assets/{,*/}*.js',
-          '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+          '<%= config.dist %>/**/*.html',
+          '<%= config.dist %>/assets/**/*.css',
+          '<%= config.dist %>/assets/**/*.js',
+          '<%= config.dist %>/assets/**/*.{png,jpg,jpeg,gif,webp,svg}'
         ]
       }
     },
@@ -49,8 +38,7 @@ module.exports = function(grunt) {
       options: {
         port: 9000,
         livereload: 35729,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
+        hostname: '0.0.0.0'
       },
       livereload: {
         options: {
@@ -63,6 +51,7 @@ module.exports = function(grunt) {
     },
 
     assemble: {
+
       pages: {
         options: {
           flatten: true,
@@ -71,22 +60,18 @@ module.exports = function(grunt) {
           data: '<%= config.src %>/data/*.{json,yml}',
           partials: '<%= config.src %>/templates/partials/*.hbs'
         },
+
         files: {
           '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
         }
       }
     },
 
-    // Before generating any new files,
+    // before generating any new files,
     // remove any previously-created files.
     clean: ['<%= config.dist %>/**/*.{html,xml}']
 
   });
-
-  grunt.loadNpmTasks('assemble');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('server', [
     'clean',
