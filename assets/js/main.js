e709d82372ba3d02a5eebc422180623ddadce505
Lucas Doyle <lucas.p.doyle@gmail.com>
2014-05-05 01:07:58 +0900

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  
+  var pass = function() {}
+  
+  var print = function(s) {
+    console.log(s)
+  }
 
   var chooseRandomImage = function() {
     return window.BG_IMAGES[Math.floor(Math.random() * window.BG_IMAGES.length)].replace('dist/', '');
@@ -6,39 +12,47 @@ $(document).ready(function() {
 
   var setBGImage = function(imageURL) {
 
-    // specifically DONT use jquery here since the image doesn't always load smoothly
+    // specifically DON'T use jquery here since the image doesn't always load smoothly
     document.body.style.backgroundImage = 'url(\'' + imageURL + '\')';
   };
+  
+  var pages = {
 
-  if (window.BASENAME == 'index') {
+    index: function() {
 
-    // disable scrolling
-    $('body').bind('touchmove', function(e) {e.preventDefault()});
-    $('body').css('overflow', 'hidden');
+      // disable scrolling
+      $('body').bind('touchmove', function(e) {e.preventDefault()});
+      $('body').css('overflow', 'hidden');
 
-    // vertical center (pity this isn't elegant with css)
-    var verticalCenter = function() {
-      var windowHeight = $(window).height();
-      var navBarHeight = $('.navbar').height();
+      // vertical center (pity this isn't elegant with css)
+      var verticalCenter = function() {
+        var windowHeight = $(window).height();
+        var landingHeight = $('.landing-navbar-wrapper').height();
 
-      var ratio = 0.6;
-      var paddingHeight = Math.abs(windowHeight - navBarHeight) / 2.0;
+        var ratio = 0.6;
+        var paddingHeight = Math.abs(windowHeight - landingHeight) / 2.0;
 
-      $('.top-padding').height(ratio * paddingHeight);
-      $('.bottom-padding').height((1.0 + (1.0 - ratio)) * paddingHeight);
-    };
+        $('.top-padding').height(ratio * paddingHeight);
+        $('.bottom-padding').height((1.0 + (1.0 - ratio)) * paddingHeight);
+      };
 
-    verticalCenter();
-    $(window).resize(verticalCenter);
+      verticalCenter();
+      $(window).resize(verticalCenter);
 
-    var interval = 6600;
-    var _setBGImage = function() {
-      var imagePath = chooseRandomImage();
-      setBGImage(imagePath);
-      setTimeout(_setBGImage, interval);
-    };
-    _setBGImage();
+      var interval = 8000;
+      var _setBGImage = function() {
+        var imagePath = chooseRandomImage();
+        setBGImage(imagePath);
+        setTimeout(_setBGImage, interval);
+      };
+      _setBGImage();
+    },
+    
+    blox: pass
+  }
 
+  if (pages.hasOwnProperty(window.BASENAME)) {
+    pages[window.BASENAME]();
   }
   else {
     setBGImage(chooseRandomImage());
