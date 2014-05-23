fda1964ba1ff25b8f37b0a5aca93557fece103e2
Lucas Doyle <lucas.p.doyle@gmail.com>
2014-05-23 13:59:11 +0900

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  
-  var pass = function() {}
-  
+
+  var pass = function() {};
+
   var print = function(s) {
-    console.log(s)
-  }
+    console.log(s);
+  };
 
   var chooseRandomImage = function() {
     return window.BG_IMAGES[Math.floor(Math.random() * window.BG_IMAGES.length)].replace('dist/', '');
@@ -15,7 +15,7 @@ $(document).ready(function() {
     // specifically DON'T use jquery here since the image doesn't always load smoothly
     document.body.style.backgroundImage = 'url(\'' + imageURL + '\')';
   };
-  
+
   var pages = {
 
     index: function() {
@@ -35,21 +35,88 @@ $(document).ready(function() {
         $('.top-padding').height(ratio * paddingHeight);
         $('.bottom-padding').height((1.0 + (1.0 - ratio)) * paddingHeight);
       };
-
       verticalCenter();
       $(window).resize(verticalCenter);
 
-      var interval = 8000;
-      var _setBGImage = function() {
-        var imagePath = chooseRandomImage();
-        setBGImage(imagePath);
-        setTimeout(_setBGImage, interval);
+      // requestAnimFrame shim
+      var requestAnimFrame = (function() {
+        return window.requestAnimationFrame ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame ||
+          window.oRequestAnimationFrame ||
+          window.msRequestAnimationFrame ||
+          function(callback, frameOffset) {
+            window.setTimeout(callback, frameOffset);
+          };
+      })();
+
+      // a nice canvas / background image based crossfading image switcher
+      var canvas = $('#canvas')[0];
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      var ctx = canvas.getContext('2d');
+      var img = new Image();
+      var alpha = 0.0;
+      var speed = 0.05;
+      var delta = speed;
+
+      // draw the canvas
+      var drawStuff = function() {
+
+        // compute new alpha
+        alpha += delta;
+
+        // draw faded image`
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = alpha;
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+        // stop fading if all the way faded in or out
+        if (alpha >= 1.0) {
+          alpha = 1.0;
+          delta = 0.0;
+        }
+        else if (alpha < 0) {
+          alpha = 0.0;
+          delta = 0.0;
+        }
+
+        // repeat forever
+        requestAnimFrame(drawStuff);
       };
-      _setBGImage();
+
+      // periodically change the image, setting the old one as the background image of the page
+      setInterval(function() {
+        setBGImage(img.src);
+        setTimeout(function() {
+          img.src = chooseRandomImage();
+          delta = -speed;
+        }, 2000);
+      }, 6000);
+      img.src = chooseRandomImage();
+      setBGImage(img.src);
+
+      // when the image is done loading, set alpha and delta to fade it in
+      img.onload = function() {
+        alpha = 0.0;
+        delta = speed;
+      };
+
+      // update canvas size
+      var resizeCanvas = function() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+      };
+      resizeCanvas();
+      $(window).resize(resizeCanvas);
+
+      // start
+      drawStuff();
     },
-    
+
     blox: pass
-  }
+  };
 
   if (pages.hasOwnProperty(window.BASENAME)) {
     pages[window.BASENAME]();
