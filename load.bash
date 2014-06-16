9136facd884e1bb55b0f1a0986dcf414d14cf689
Lucas Doyle <lucas.p.doyle@gmail.com>
2014-06-16 17:52:41 +0900

    misc

--- load.bash
+++ load.bash
@@ -1,6 +1,6 @@
 find contents/js/ -type f -name "*.js" -exec geany '{}' \; &
 find contents/less/ -type f -name "*.less" -exec geany '{}' \; &
-find contents/articles/ -type f -name "*.md" -exec geany '{}' \; &
+find contents/posts/ -type f -name "*.md" -exec geany '{}' \; &
 find templates/ -type f -name "*.jade" -exec geany '{}' \; &
 geany contents/*.json &
 geany contents/*.md &
