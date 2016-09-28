2afad9817eda4a5349986eaff166c6dddef48bdb
Lucas Doyle <ldoyle@airware.com>
2016-09-28 07:09:52 -0700

    Stop Tags from crashing page if no tags

--- components/Tags.js
+++ components/Tags.js
@@ -4,24 +4,22 @@ import { tagMap, capitalizeFirstLetter } from 'utils';
 import { prefixLink } from 'gatsby-helpers';
 
 const Tags = props => {
-  return (
-    <div {...props}>
-      {(props.post.tags || []).length ?
-        (
-          <span>
-            Tags: {props.post.tags.map((tag, i) => {
-              return (
-               <Link key={i} to={{ pathname: prefixLink('/tags/'), hash: '#'+tagMap(tag) }}>
-                 {capitalizeFirstLetter(tag)}
-               </Link>
-            );
-            }).reduce((accu, elem) => {
-              return accu === null ? [elem] : [...accu, ' | ', elem];
-            }, null)}
-          </span>
-        ) : null}
-    </div>
-  );
+  return (props.post && props.post.tags || []).length ?
+    (
+      <div {...props}>
+        <span>
+          Tags: {props.post.tags.map((tag, i) => {
+            return (
+             <Link key={i} to={{ pathname: prefixLink('/tags/'), hash: '#'+tagMap(tag) }}>
+               {capitalizeFirstLetter(tag)}
+             </Link>
+          );
+          }).reduce((accu, elem) => {
+            return accu === null ? [elem] : [...accu, ' | ', elem];
+          }, null)}
+        </span>
+      </div>
+  ) : null;
 };
 
 export default Tags;
