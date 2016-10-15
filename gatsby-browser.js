31226ee5bbdcd6317085a71dd0079a27d7ae6117
Lucas Doyle <ldoyle@airware.com>
2016-10-14 23:22:28 -0700

    Resize youtube iframes properly

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,24 @@
 import ReactGA from 'react-ga';
 import { config, pages } from 'config';
 import find from 'lodash/find';
+import _ from 'lodash';
+import $ from 'jquery';
 
 import { browserHistory } from 'react-router';
 
 if (config.gaCode) {
   ReactGA.initialize(config.gaCode);
 }
+
+// force 16 / 9 resolution for all videos
+const resizeVideos = _.debounce(function() {
+  $("iframe[src^='http://www.youtube.com']").each(function () {
+    const $el = $(this);
+    $el.height($el.width() * 9 / 16);
+  });
+}, 100)
+
+$(window).resize(resizeVideos);
 
 exports.onRouteUpdate = state => {
   const page = find(pages, { path: state.pathname });
@@ -21,4 +33,6 @@ exports.onRouteUpdate = state => {
       page: state.pathname,
     });
   }
+
+  resizeVideos()
 };
