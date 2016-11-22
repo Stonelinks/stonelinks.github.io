007734346b7f0fa8a321cb5d896dee43fadc7bf1
Lucas Doyle <ldoyle@airware.com>
2016-11-21 20:13:44 -0800

    Bring back the misc folder, fix links to static files

--- utils/index.js
+++ utils/index.js
@@ -8,8 +8,14 @@ import moment from 'moment';
 export function fixLinks (ref, router) {
   catchLinks(ref, (href) => {
     const ext = href.split('.').pop().toLowerCase();
-    if (['zip', 'png', 'jpg', 'txt', 'md'].indexOf(ext) === -1) {
+    if (['zip', 'png', 'jpg', 'jpeg', 'txt', 'md', 'pdf'].indexOf(ext) === -1) {
       router.push(href);
+    } else {
+      const link = document.createElement('a');
+      link.href = href;
+      link.target = '_blank';
+      document.body.appendChild(link);
+      link.click();
     }
   });
 }
