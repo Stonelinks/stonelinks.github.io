0a77f5112eaf69d40053b965db6c6c2434b69bb6
Lucas Doyle <lucas.p.doyle@gmail.com>
2025-06-24 09:04:23 -0700

    feat: add dateFormat prop to DateDisplay component
    - Added dateFormat prop to customize date format in DateDisplay component.

--- app/components/Date.tsx
+++ app/components/Date.tsx
@@ -1,10 +1,21 @@
 import commonStyles from './common.module.css';
 import { format } from 'date-fns';
 
-export const DateDisplay = ({ date }: { date?: string }) => {
+export const DateDisplay = ({
+  date,
+  dateFormat,
+}: {
+  date?: string;
+  dateFormat?: string;
+}) => {
+  let df = 'MMMM d, yyyy';
+  if (dateFormat) {
+    df = dateFormat;
+  }
+
   return (
     <p className={commonStyles.date}>
-      {date ? format(new Date(date), 'MMMM d, yyyy') : 'Date unknown'}
+      {date ? format(new Date(date), df) : 'Date unknown'}
     </p>
   );
 };
