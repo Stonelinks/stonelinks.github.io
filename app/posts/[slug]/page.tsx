e345471d9979cbf1a66de21c8790454e5532f947
Lucas Doyle <lucas.p.doyle@gmail.com>
2025-06-25 14:38:28 -0700

    feat: add metadata generation for pages
    - Added `generateMetadata` functions for various pages
    - Created `DEFAULT_TITLE` constant in `lib/const.ts`

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { getAllPostSlugs, getPostBySlug } from '../../../lib/posts';
 import TagList from '@/components/TagList';
 import { DateDisplay } from '@/components/Date';
 import Lightbox from '@/components/Lightbox';
+import type { Metadata } from 'next';
+import { DEFAULT_TITLE } from '../../../lib/const';
 
 interface PostProps {
   params: { slug: string };
@@ -69,4 +71,25 @@ export async function generateStaticParams() {
     // For some reason we need to keep both encoded and unencoded versions of the slug, otherwise we break either the local dev experience or the static site generator when we publish.
     .flatMap((slug) => [encodeURIComponent(slug), slug]);
   return Array.from(new Set(slugs)).map((slug) => ({ slug }));
+}
+
+/**
+ * Generate metadata for the post page
+ */
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const post = await getPostBySlug(params.slug);
+
+  return {
+    title: `${post.metadata.title} - ${DEFAULT_TITLE}`,
+    description: post.metadata.excerpt || 'Read this post by Lucas Doyle',
+    openGraph: {
+      title: post.metadata.title,
+      description: post.metadata.excerpt,
+      images: [post.metadata.featuredImage || ''],
+    },
+  };
 }
