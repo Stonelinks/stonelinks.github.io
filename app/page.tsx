d323498487b87df8e46865d80811c0d167644cf4
Lucas Doyle <lucas.p.doyle@gmail.com>
2025-06-24 22:33:56 -0700

    feat: add AllProjectsList component
    - Added AllProjectsList component to Home page
    - Displayed 5 projects and 5 posts on the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { AllPostsList } from './components/AllPostsList';
+import { AllProjectsList } from './components/AllProjectsList';
 import { PageWrapper } from './components/PageWrapper';
 
 const Home = () => {
   return (
     <PageWrapper>
-      <AllPostsList />
+      <AllProjectsList limit={5} />
+      <AllPostsList limit={5} />
     </PageWrapper>
   );
 };
